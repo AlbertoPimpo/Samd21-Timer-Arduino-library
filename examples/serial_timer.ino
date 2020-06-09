8d73d6cf637561cf3464babefc254036adb31669
Alberto Pimpo <alberto.pimpo@gmail.com>
2020-06-09 22:48:36 +0200

    Added serial timer example

---
+++ examples/serial_timer.ino
@@ -0,0 +1,15 @@
+#include <Samd21Timer.h>
+
+
+
+void callback(){
+  SerialUSB.println("Test");
+}
+
+void setup() {
+}
+
+void loop() {
+  Timer.enable(TIMER_5, 1.0, &callback);
+  //Timer.disable(TIMER_5);
+}
